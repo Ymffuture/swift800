5b9964a62394d8b846299a1ad4f7693a40197f0b
Kgomotso Leonard Nkosi <148737876+Ymffuture@users.noreply.github.com>
2025-11-16 05:23:17 +0200

    Update Contract.ts

--- models/Contract.ts
+++ models/Contract.ts
@@ -1,51 +1,48 @@
 // models/Contract.ts
 import mongoose, { Schema, Document } from 'mongoose';
 
-
 export interface ISigner {
-name: string;
-email?: string;
-signedAt?: string;
-signatureUrl?: string; // cloudinary url
-ip?: string;
-userAgent?: string;
-idNumber?: string;
-idPhotoUrl?: string;
+  name?: string;
+  email?: string;
+  signedAt?: string;
+  signatureUrl?: string;
+  ip?: string;
+  userAgent?: string;
+  idNumber?: string;
+  idPhotoUrl?: string;
 }
-
 
 export interface IContract extends Document {
-shortCode: string;
-title: string;
-body: string;
-status: 'pending' | 'partially_signed' | 'signed';
-signerA: ISigner;
-signerB?: ISigner;
-createdAt: string;
+  shortCode: string;
+  title: string;
+  body: string;
+  status: 'pending' | 'partially_signed' | 'signed';
+  signerA: ISigner;
+  signerB?: ISigner;
+  createdAt: string;
+  pdfUrl?: string;
 }
 
-
 const SignerSchema = new Schema({
-name: String,
-email: String,
-signedAt: String,
-signatureUrl: String,
-ip: String,
-userAgent: String,
-idNumber: String,
-idPhotoUrl: String
-});
-
+  name: String,
+  email: String,
+  signedAt: String,
+  signatureUrl: String,
+  ip: String,
+  userAgent: String,
+  idNumber: String,
+  idPhotoUrl: String
+}, { _id: false });
 
 const ContractSchema = new Schema({
-shortCode: { type: String, required: true, index: true, unique: true },
-title: String,
-body: String,
-status: { type: String, default: 'pending' },
-signerA: { type: SignerSchema, required: true },
-signerB: { type: SignerSchema },
-createdAt: { type: String, default: () => new Date().toISOString() }
+  shortCode: { type: String, required: true, index: true, unique: true },
+  title: String,
+  body: String,
+  status: { type: String, default: 'pending' },
+  signerA: { type: SignerSchema, required: true },
+  signerB: { type: SignerSchema },
+  createdAt: { type: String, default: () => new Date().toISOString() },
+  pdfUrl: String
 });
-
 
 export default mongoose.models.Contract || mongoose.model<IContract>('Contract', ContractSchema);
