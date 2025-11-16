c0ea403dd972f9456deece4e624bca69c4fd2110
Kgomotso Leonard Nkosi <148737876+Ymffuture@users.noreply.github.com>
2025-11-16 04:56:54 +0200

    Update utils.ts

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,10 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
-
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+// lib/utils.ts
+export function makeShortCode(length = 8) {
+const nums = '0123456789';
+let out = '';
+for (let i = 0; i < length; i++) out += nums[Math.floor(Math.random() * nums.length)];
+return out;
 }
+
+
+export function nowISO() { return new Date().toISOString(); }
