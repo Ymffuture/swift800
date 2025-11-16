20137ed8118eafb626ac60b9ece3743689b61ca3
Kgomotso Leonard Nkosi <148737876+Ymffuture@users.noreply.github.com>
2025-11-16 07:33:00 +0200

    Update code.ts

--- pages/api/contracts/code.ts
+++ pages/api/contracts/code.ts
@@ -1,10 +1,12 @@
 // pages/api/contracts/code.ts
 import type { NextApiRequest, NextApiResponse } from 'next';
-import dbConnect from '../../../lib/db';
+import { connectToDB } from '../../../lib/db';
+
 import Contract from '../../../models/Contract';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  await dbConnect();
+  await connectToDB();
+
   const code = String(req.query.code || '');
   if (!code) return res.status(400).json({ ok: false, message: 'code required' });
   const contract = await Contract.findOne({ shortCode: code }).lean();
